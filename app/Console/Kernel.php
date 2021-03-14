d39bbef61c1cf6679fd510efa67a884efc8d49ad
Henry Whitaker <henrywhitaker3@outlook.com>
2021-03-14 23:40:39 +0000

    Use right schedule thing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->action(DeleteOldChecksAction::class)->everyHour();
+        $schedule->action(DeleteOldChecksAction::class)->hourly();
     }
 
     /**
