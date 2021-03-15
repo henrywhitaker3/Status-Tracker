5e39d41a34d36b8ce352412e3106c14681436fde
Henry Whitaker <henrywhitaker3@outlook.com>
2021-03-15 00:22:01 +0000

    Add event/listener scaffolding to handle notifications in future

---
+++ app/Events/ServiceCheckFailedEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Service;
+use App\Models\ServiceCheck;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ServiceCheckFailedEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Service $service;
+
+    public ServiceCheck $serviceCheck;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Service $service, ServiceCheck $serviceCheck)
+    {
+        $this->service = $service;
+        $this->serviceCheck = $serviceCheck;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Listeners/ServiceCheckFailedListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class ServiceCheckFailedListener
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
