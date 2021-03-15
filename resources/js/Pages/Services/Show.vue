49fd352a9a635851534d8067db873a16337bbb52
Henry Whitaker <henrywhitaker3@outlook.com>
2021-03-15 19:49:43 +0000

    Re-work UI on show

--- resources/js/Pages/Services/Show.vue
+++ resources/js/Pages/Services/Show.vue
@@ -1,37 +1,21 @@
 <template>
 <div>
     <Layout>
-        <div class="mb-6">
-            <inertia-link href="/services" class="button button-primary mr-2 mb-2">Back to All Services</inertia-link>
+        <div class="mb-3">
+            <inertia-link nostyle href="/services" class="button button-primary mr-2 mb-2">Back to All Services</inertia-link>
             <button @click="destroy" class="button button-danger">Delete</button>
         </div>
         <div class="flex flex-col space-y-4">
-            <div class="card flex flex-col space-y-1">
-                <h1>{{ service.name }}</h1>
-                <h2>{{ service.status ? 'Up' : 'Down' }} for {{ prettyDiff(service.status_changed_at) }}</h2>
+            <div class="card block sm:flex justify-between space-y-1">
+                <div>
+                    <h1>{{ service.name }}</h1>
+                    <h2>{{ service.status ? 'Up' : 'Down' }} for {{ prettyDiff(service.status_changed_at) }}</h2>
+                </div>
                 <UptimeGraph :checks="checks" />
             </div>
             <div class="card">
                 <h2>Checks</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Type</th>
-                            <th>Up</th>
-                            <th>Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            v-for="check in checks"
-                            :key="check.id"
-                        >
-                            <td>{{ check.type }}</td>
-                            <td>{{ check.up ? 'Yes' : 'No' }}</td>
-                            <td>{{ prettyDiff(check.created_at) }} ago</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <ChecksTable :checks="checks" />
             </div>
         </div>
     </Layout>
@@ -41,11 +25,13 @@
 <script>
 import Layout from '../../components/Layout';
 import UptimeGraph from '../../components/UptimeGraph';
+import ChecksTable from './components/ChecksTable';
 
 export default {
     components: {
         Layout,
         UptimeGraph,
+        ChecksTable,
     },
     props: {
         service: Object,
---
+++ resources/js/Pages/Services/components/ChecksTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Up</th>
+                <th>Time</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+                v-for="check in checks"
+                :key="check.id"
+            >
+                <td>{{ check.type }}</td>
+                <td>{{ check.up ? 'Yes' : 'No' }}</td>
+                <td>{{ prettyDiff(check.created_at) }} ago</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        checks: Array
+    }
+}
+</script>
