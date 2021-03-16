71733368067bb0882a2474374227a35c8aa573bd
Henry Whitaker <henrywhitaker3@outlook.com>
2021-03-16 12:32:14 +0000

    Add icons for up/down

--- resources/js/Pages/Services/components/ChecksTable.vue
+++ resources/js/Pages/Services/components/ChecksTable.vue
@@ -13,8 +13,11 @@
                 v-for="check in checks.data"
                 :key="check.id"
             >
-                <td>{{ check.type }}</td>
-                <td>{{ check.up ? 'Yes' : 'No' }}</td>
+                <td><span class="solo-type">{{ check.type }}</span></td>
+                <td class="text-2xl">
+                    <font-awesome-icon v-if="check.up" class="text-green-400 " icon="check-circle" />
+                    <font-awesome-icon v-else class="text-red-500 " icon="times-circle" />
+                </td>
                 <td>{{ prettyDiff(check.created_at) }} ago</td>
                 <td class="text-right w-px">
                     <inertia-link :href="'/checks/' + check.id"><font-awesome-icon icon="ellipsis-v" /></inertia-link>
--- resources/js/icons.js
+++ resources/js/icons.js
@@ -1,8 +1,12 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
     faCircle,
-    faEllipsisV
+    faEllipsisV,
+    faCheckCircle,
+    faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faCircle);
 library.add(faEllipsisV);
+library.add(faCheckCircle);
+library.add(faTimesCircle);
